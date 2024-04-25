18143678867ea79e406fb65fabc2db8faf50d374
RobertM006 <144116774+RobertM006@users.noreply.github.com>
2024-04-25 09:29:08 +0200

    nu

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 function toggleVisibility(id) {
-    var element = document.getElementById(id);
-    if (element.style.display === 'none') {
-      element.style.display = 'block';
-    } else {
-      element.style.display = 'none';
-    }
+  var element = document.getElementById(id);
+  if (element.style.display === 'none') {
+    element.style.display = 'block';
+  } else {
+    element.style.display = 'none';
   }
+}
+
+function highlightBlocks() {
+  var blok = document.querySelectorAll('.oorzaakEen');
+
+  blok.forEach(function(element) {
+      element.style.backgroundColor = 'green';
+  });
+}
+
+function highlightBlocks2() {
+var blok = document.querySelectorAll('.oorzaakTwee');
+
+blok.forEach(function(element) {
+    element.style.backgroundColor = 'green';
+});
+}
+
+function highlightBlocks3() {
+var blok = document.querySelectorAll('.oorzaakDrie');
+
+blok.forEach(function(element) {
+    element.style.backgroundColor = 'green';
+});
+}
+
+function highlightBlocks4() {
+var blok = document.querySelectorAll('.oorzaakVier');
+
+blok.forEach(function(element) {
+    element.style.backgroundColor = 'green';
+});
+}
+
+function unhighlightBlocks() {
+var blok = document.querySelectorAll('.oorzaakEen, .oorzaakTwee, .oorzaakDrie, .oorzaakVier');
+
+blok.forEach(function(element) {
+    element.style.backgroundColor = 'white';
+});
+}
+
+
+
+
+
+
