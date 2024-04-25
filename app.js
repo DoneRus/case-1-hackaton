ef7baebf6690b405ef1dc0abcc4bad01eb44683c
Stefanostikkie <stefanostikkie@gmail.com>
2024-04-25 14:31:49 +0200

    !!!

--- app.js
+++
@@ -1,7 +0,0 @@
-function overlayOff(overlays) {
-  document.getElementById(overlays).style.display = "none";
-}
-
-function overlayOn(overlays) {
-  document.getElementById(overlays).style.display = "block";
-}
