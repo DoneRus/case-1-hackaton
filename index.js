1872c87b7248f9920d80c2e6eeaeb5ff9051ef46
RobertM006 <144116774+RobertM006@users.noreply.github.com>
2024-04-25 12:41:16 +0200

    reset weg op klik kleur aan en uit

--- index.js
+++ index.js
@@ -1,55 +1,83 @@
-function unhighlightBlocks() {
-let blok = document.querySelectorAll('.oorzaakEen, .oorzaakTwee, .oorzaakDrie, .oorzaakVier, .oorzaakEen2, .oorzaakTwee2, .oorzaakDrie2, .oorzaakVier2');
-
-blok.forEach(function(element) {
-    element.style.backgroundColor = 'white';
-});
-}
-
 function highlightBlocks() {
   let blok = document.querySelectorAll('.oorzaakEen');
   let blok2 = document.querySelectorAll('.oorzaakEen2');
   
   blok.forEach(function(element) {
+    if (element.style.backgroundColor === 'rgb(225, 91, 109)') {
+      element.style.backgroundColor = 'white';
+    } else {
       element.style.backgroundColor = 'rgb(225, 91, 109)';
+    }
   });
+  
   blok2.forEach(function(element) {
-    element.style.backgroundColor = 'rgb(235, 178, 33)';
+    if (element.style.backgroundColor === 'rgb(235, 178, 33)') {
+      element.style.backgroundColor = 'white';
+    } else {
+      element.style.backgroundColor = 'rgb(235, 178, 33)';
+    }
   });
-  }
+}
 
 function highlightBlocks2() {
   let blok = document.querySelectorAll('.oorzaakTwee');
   let blok2 = document.querySelectorAll('.oorzaakTwee2');
   
   blok.forEach(function(element) {
+    if (element.style.backgroundColor === 'rgb(225, 91, 109)') {
+      element.style.backgroundColor = 'white';
+    } else {
       element.style.backgroundColor = 'rgb(225, 91, 109)';
+    }
   });
+  
   blok2.forEach(function(element) {
-    element.style.backgroundColor = 'rgb(235, 178, 33)';
+    if (element.style.backgroundColor === 'rgb(235, 178, 33)') {
+      element.style.backgroundColor = 'white';
+    } else {
+      element.style.backgroundColor = 'rgb(235, 178, 33)';
+    }
   });
-  }
+}
 
 function highlightBlocks3() {
   let blok = document.querySelectorAll('.oorzaakDrie');
   let blok2 = document.querySelectorAll('.oorzaakDrie2');
   
   blok.forEach(function(element) {
+    if (element.style.backgroundColor === 'rgb(225, 91, 109)') {
+      element.style.backgroundColor = 'white';
+    } else {
       element.style.backgroundColor = 'rgb(225, 91, 109)';
+    }
   });
+  
   blok2.forEach(function(element) {
-    element.style.backgroundColor = 'rgb(235, 178, 33)';
+    if (element.style.backgroundColor === 'rgb(235, 178, 33)') {
+      element.style.backgroundColor = 'white';
+    } else {
+      element.style.backgroundColor = 'rgb(235, 178, 33)';
+    }
   });
-  }
+}
 
 function highlightBlocks4() {
-let blok = document.querySelectorAll('.oorzaakVier');
-let blok2 = document.querySelectorAll('.oorzaakVier2');
-
-blok.forEach(function(element) {
-    element.style.backgroundColor = 'rgb(225, 91, 109)';
-});
-blok2.forEach(function(element) {
-  element.style.backgroundColor = 'rgb(235, 178, 33)';
-});
+  let blok = document.querySelectorAll('.oorzaakVier');
+  let blok2 = document.querySelectorAll('.oorzaakVier2');
+  
+  blok.forEach(function(element) {
+    if (element.style.backgroundColor === 'rgb(225, 91, 109)') {
+      element.style.backgroundColor = 'white';
+    } else {
+      element.style.backgroundColor = 'rgb(225, 91, 109)';
+    }
+  });
+  
+  blok2.forEach(function(element) {
+    if (element.style.backgroundColor === 'rgb(235, 178, 33)') {
+      element.style.backgroundColor = 'white';
+    } else {
+      element.style.backgroundColor = 'rgb(235, 178, 33)';
+    }
+  });
 }
